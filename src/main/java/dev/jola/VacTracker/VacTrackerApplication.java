2975c24f618b90bbc71122703df22d3f0983a0f9
aleksandar-jolic <coajolic@gmail.com>
2023-02-18 22:40:55 +0100

    Connected to MongoDB

--- src/main/java/dev/jola/VacTracker/VacTrackerApplication.java
+++ src/main/java/dev/jola/VacTracker/VacTrackerApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VacTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VacTrackerApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(VacTrackerApplication.class, args);}
 
 }
