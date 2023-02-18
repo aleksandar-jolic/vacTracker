69416b756b9c699970ddad070e415d2398a9604c
aleksandar-jolic <coajolic@gmail.com>
2023-02-18 15:40:43 +0100

    Initial commit

---
+++ src/main/java/dev/jola/VacTracker/VacTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.jola.VacTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jola/VacTracker/VacTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jola.VacTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
